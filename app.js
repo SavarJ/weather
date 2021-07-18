a8369d6cdc7e8f54e9826a5e2fc91d84a65b06ee
Savar Jain: HomeLaptop <jainsavar@gmail.com>
2021-07-17 21:55:40 -0400

    Add code to the newly created files. Still needs some bug fixes

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const https = require("https");
 const ejs = require("ejs");
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/", function (req, res) {
   console.log("Someone just opened the root directory! :)");
-  res.sendFile(__dirname + "/index.html");
+  res.render("search");
 });
 
 app.get("/retry", function (req, res) {
@@ -40,10 +49,6 @@ function displayWeather(req, res) {
   https.get(url, function (httpResponse) {
     console.log(httpResponse.statusCode);
     console.log(url);
-    //Styling code
-    res.write(
-      "<style> body { background-color: teal; color: #fff; font-family: Verdana, sans-serif; padding: 5% } </style>"
-    );
 
     //Success
     if (httpResponse.statusCode === 200) {
@@ -61,82 +66,41 @@ function displayWeather(req, res) {
         city = weatherData.name + ", ";
         country = weatherData.sys.country;
 
-        res.write(
-          "<h1> The weather is currently: " +
-            weatherMain +
-            " - " +
-            weatherDesc +
-            " </h1>"
-        );
-        //Only include state if they entered one
-        res.write(
-          "<h1>The temperature in " +
-            city +
-            " " +
-            (enteredState ? state + " " : "") +
-            country +
-            " is " +
-            temp +
-            " &#176;" +
-            letter +
-            " </h1>"
-        );
         const realFeelLine = "Real Feel: " + realFeel + " &#176;" + letter;
         const minTempLine = "Min: " + minTemp + " &#176;" + letter;
         const maxTempLine = "Max: " + maxTemp + " &#176;" + letter;
-
-        res.write(
-          "<h1>" +
-            realFeelLine +
-            ", " +
-            minTempLine +
-            ", " +
-            maxTempLine +
-            " </h1>"
-        );
 
         const weatherIconUrl =
           "http://openweathermap.org/img/wn/" +
           weatherData.weather[0].icon +
           "@2x.png";
-        res.write(
-          "<img src=" +
-            weatherIconUrl +
-            " alt='weather-img' style='width: 25%' >"
-        );
 
-        //Search again button css
-        res.write(
-          "<style>button{height: 50px; width: max-content; border-radius: 50px; background-color: rgb(67, 67, 231); cursor: pointer; font-size: 1.5rem; border: 2px solid black; color: rgb(228, 233, 245); position: relative; bottom: 150px;left: 50px; padding: 10px;}button:hover{background-color: rgb(70, 33, 156);}form{width: max-content;display: inline-block;}</style>"
-        );
+        const weatherDescLine =
+          "The weather is currently: " + weatherMain + " - " + weatherDesc;
+        const mainTempLine =
+          "The temperature in " +
+          city +
+          " " +
+          (enteredState ? state + " " : "") +
+          country +
+          " is " +
+          temp +
+          " &#176;" +
+          letter;
+        const moreTempLine =
+          realFeelLine + ", " + minTempLine + ", " + maxTempLine;
+        const ejsobj = {
+          weatherDescLine: weatherDescLine,
+          mainTempLine: mainTempLine,
+          moreTempLine: moreTempLine,
+          weatherIconUrl: weatherIconUrl,
+        };
+
+        res.render("weather", ejsobj);
       });
     } else {
-      //Some problem
-      res.write("<h1> Uh Oh :( </h1>");
-
-      res.write("<h1>This place does not exist yet :) </h1>");
-
-      //Seach again button css
-      res.write(
-        "<style>button{height: 50px; width: max-content; border-radius: 50px; background-color: rgb(67, 67, 231); cursor: pointer; font-size: 1.5rem; border: 2px solid black; color: rgb(228, 233, 245); padding: 10px;}button:hover{background-color: rgb(70, 33, 156);}form{width: max-content; display: inline-block;}</style>"
-      );
+      res.render("notfound");
     }
-    //Search again button
-    setTimeout(function () {
-      res.write(
-        "<form action='/retry' method='GET'><button type='submit'>SEARCH AGAIN</button></form>"
-      );
-    }, 700);
-    //Footer
-    setTimeout(function () {
-      res.write(
-        "<style> footer{font-size:.8rem;text-align:center;color:#e0e0e0}#my{text-decoration:none;color:#42e3e9}#my:hover{font-weight:700;color:#8ce7eb} </style>"
-      );
-      res.write(
-        "<footer><script crossorigin=anonymous src=https://kit.fontawesome.com/68e357ed07.js></script><p class=last-line>Made with <i class='fa-heart fas'id=heart></i> by <a href=https://www.jainsavar.com id=my rel='noopener noreferrer'target=_blank>Savar Jain</a></footer>"
-      );
-      res.send();
-    }, 750);
   });
 }
 
